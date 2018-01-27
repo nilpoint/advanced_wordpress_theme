b1b7847d4274003df63977ac064dab133ec58226
John Smith <nilpoint@hotmail.com>
2018-01-27 18:15:12 +0800

    Add a heading on what type of archive

--- archive.php
+++ archive.php
@@ -2,6 +2,27 @@
 
 <div class="container content">
   <div class="main block">
+    <h1 class="page-header">
+      <?php
+        if (is_category(  )) {
+          single_cat_title(  );
+        } else if(is_author(  )) {
+          the_post(  );
+          echo '按作者分类：' . get_the_author(  );
+          rewind_posts(  );
+        } elseif(is_tag(  )){
+          single_tag_title(  );
+        } elseif(is_day(  )){
+          echo '按天分类：' . get_the_date(  );
+        } elseif(is_month(  )){
+          echo '按月分类：' . get_the_date( 'Y-m' );
+        } elseif(is_year(  )){
+          echo '按年分类：' . get_the_date( 'Y' );
+        } else {
+          echo '档案';
+        }        
+      ?>
+    </h1>
     <?php if (have_posts(  )) : ?>
       <?php while(have_posts(  )) : the_post(  ); ?>
         <div class="archive-post">

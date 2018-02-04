db273302806ce0e2d81b4ac9dad46409b08fa05c
John Smith <nilpoint@hotmail.com>
2018-02-04 16:27:33 +0800

    Support the theme widget

--- functions.php
+++ functions.php
@@ -12,6 +12,19 @@
   }
   add_action( 'after_setup_theme', 'adv_theme_support' );
 
+  // Widget Locations
+  function init_widgets($id){
+    register_sidebar( array(
+      'name' => 'Sidebar', 
+      'id' => 'sidebar',
+      'before_widget' => '<div class="block side-widget">',
+      'after_widget' => '</div>',
+      'before_title' => '<h3>',
+      'after_title' => '</h3>'
+    ));
+  }
+  add_action( 'widgets_init', init_widgets );
+
   // Post Format Support
   add_theme_support( 'post-formats', array('aside', 'gallery', 'link') );
 

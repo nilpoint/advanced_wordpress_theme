d35a54302a52b3043bc44e7325aa26f76022422e
John Smith <nilpoint@hotmail.com>
2018-02-04 17:14:04 +0800

    Add three more box widgets

--- front-page.php
+++ front-page.php
@@ -36,8 +36,17 @@
       <?php echo wpautop( 'Sorry, no posts were found' ); ?>
     <?php endif; ?>
   </div>
+  <?php if(is_active_sidebar( 'box1' )) : ?>
+    <?php dynamic_sidebar( 'box1' ); ?>
+  <?php endif;?>
+  <?php if(is_active_sidebar( 'box2' )) : ?>
+    <?php dynamic_sidebar( 'box2' ); ?>
+  <?php endif;?>
+  <?php if(is_active_sidebar( 'box3' )) : ?>
+    <?php dynamic_sidebar( 'box3' ); ?>
+  <?php endif;?>
 </div>
-  
+
 <?php get_footer(  ); ?>
 
 <?php
--- functions.php
+++ functions.php
@@ -30,6 +30,30 @@
       'before_title' => '<h1>',
       'after_title' => '</h1>'
     ));
+    register_sidebar( array(
+      'name' => 'Box 1', 
+      'id' => 'box1',
+      'before_widget' => '<div class="box box1">',
+      'after_widget' => '</div>',
+      'before_title' => '<h3>',
+      'after_title' => '</h3>'
+    ));
+    register_sidebar( array(
+      'name' => 'Box 2', 
+      'id' => 'box2',
+      'before_widget' => '<div class="box box2">',
+      'after_widget' => '</div>',
+      'before_title' => '<h3>',
+      'after_title' => '</h3>'
+    ));
+    register_sidebar( array(
+      'name' => 'Box 3', 
+      'id' => 'box3',
+      'before_widget' => '<div class="box box3">',
+      'after_widget' => '</div>',
+      'before_title' => '<h3>',
+      'after_title' => '</h3>'
+    ));
   }
   add_action( 'widgets_init', init_widgets );
 

525738d4e24ebe88ecb5cca95a301562243b3883
John Smith <nilpoint@hotmail.com>
2018-02-05 21:03:58 +0800

    Add a comment form with some style

--- comments.php
+++ comments.php
@@ -20,3 +20,20 @@
 ); ?>
 
 <?php wp_list_comments($args, $comments); ?>
+
+<?php
+$form_args = array(
+  // change the title of send button 
+  'label_submit'=>'Send',
+  // change the title of the reply section
+  'title_reply'=>'Write a Reply or Comment',
+  // remove "Text or HTML to be displayed after the set of comment 
+  // fields"
+  'comment_notes_after' => '',
+  // redefine your own textarea (the comment body)
+  'comment_field' => '<p class="comment-form-comment">
+  <label for="comment">' . _x( 'Comment', 'noun' ) . '</label>
+  <br /><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true">
+  </textarea></p>',
+); ?>
+<?php comment_form( $form_args ); ?>
